b85b201feb99e1218e333b10a71e65c415e41f35
Blackbrood <96950354+Blackbrood@users.noreply.github.com>
2024-02-23 12:14:10 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = 'elisa_md@;;;ZFNSRJpI#cAvTNoQwIArMFvpYUqrwyqrxbTKenEvThIWf4_8_Y40'
+var SESSION_ID = 'elisa_md@;;;4M0AESgb#eY9YjYmRFhBx6QvkjVBaVLQdjjEoPW4deKpWbPeiDi4'
 global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
